22cfd087fa4674e0431f817cb491649eb1bfa533
yiuyiuyiu2004 <132914667+yiuyiuyiu2004@users.noreply.github.com>
2023-05-08 21:38:33 +0800

    unity?

---
+++ HistoKwen/HistoKwen2/bruh/CategoryBtnScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CategoryBtnScript : MonoBehaviour
+{
+    [SerializeField] private Text categoryTitleText; //this is a variable that give the title on difficulty selection like easy, moderate, difficult
+    [SerializeField] private Text scoreText; //a variable for score to appear
+    [SerializeField] private Button btn; //this makes the button for difficulty selection
+
+    public Button Btn { get => btn; }
+
+    public void SetButton(string title, int totalQuestion)
+    {
+        categoryTitleText.text = title;
+        scoreText.text = PlayerPrefs.GetInt(title, 0) + "/" + totalQuestion; //we get the score save for this category
+    }
+
+}
---
+++ HistoKwen/HistoKwen2/bruh/QuizDataScriptable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this code makes a certain thing to put the questions and answers
+[CreateAssetMenu(fileName = "QuestionsData", menuName = "QuestionsData", order = 1)]
+public class QuizDataScriptable : ScriptableObject
+{
+    public string categoryName;
+    public List<Question> questions;
+}
